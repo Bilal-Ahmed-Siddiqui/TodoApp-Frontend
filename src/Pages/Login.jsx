a7e33940e5fc3bfbb680d21519acf49ab2b388b0
Bilal Ahmed <bilalkietacc@gmail.com>
2024-10-19 20:46:06 +0500

    added tailwind. login and signup page ui completed

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
-
+import React from "react";
+import loginPic from "../assets/login-illustration.png";
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+      <div
+        className="m-10 rounded-lg shadow-lg bg-[#FFE6DF]"
+        style={{ margin: "5%" }}
+      >
+        <div className="flex">
+          <div className="w-1/2">
+            <div className="bg-gray-100 p-6 flex flex-col items-center justify-center min-h-screen">
+              <h2 className="text-4xl font-bold mb-2">Todo Application</h2>
+              <p className="text-lg text-gray-500 mb-6">Just Todo it</p>
 
-export default Login
+              <h2 className="text-2xl font-semibold mb-4 mt-32">Login</h2>
+              <input
+                type="email"
+                placeholder="email"
+                className="mb-4 p-2 border border-gray-300 rounded w-64"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                className="mb-4 p-2 border border-gray-300 rounded w-64"
+              />
+              <button className="bg-[#8681FF] text-white py-2 px-6 rounded hover:bg-[#7a76eb] mb-4">
+                Login
+              </button>
+
+              <p className="text-sm text-gray-600">
+                Don't have an account?{" "}
+                <span className="text-[#8681FF] cursor-pointer">
+                  Sign up now
+                </span>
+              </p>
+            </div>
+          </div>
+          <div className="w-1/2">
+            {/* Section 2 content */}
+            <img src={loginPic} alt="" />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
+import loginPic from "../assets/login-illustration.png";
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <>
+    <div
+      className="m-10 rounded-lg shadow-lg bg-[#FFE6DF]"
+      style={{ margin: "5%" }}
+    >
+      <div className="flex">
+        <div className="w-1/2">
+          <div className="bg-gray-100 p-6 flex flex-col items-center justify-center min-h-screen">
+            <h2 className="text-4xl font-bold mb-2">Todo Application</h2>
+            <p className="text-lg text-gray-500 mb-6">Just Todo it</p>
+
+            <h2 className="text-2xl font-semibold mb-4 mt-32">Sign up</h2>
+            <input
+              type="test"
+              placeholder="username"
+              className="mb-4 p-2 border border-gray-300 rounded w-64"
+            />
+            <input
+              type="email"
+              placeholder="email"
+              className="mb-4 p-2 border border-gray-300 rounded w-64"
+            />
+            <input
+              type="password"
+              placeholder="Password"
+              className="mb-4 p-2 border border-gray-300 rounded w-64"
+            />
+            <button className="bg-[#8681FF] text-white py-2 px-6 rounded hover:bg-[#7a76eb] mb-4">
+              Sign up
+            </button>
+
+            <p className="text-sm text-gray-600">
+              Already have an account?{" "}
+              <span className="text-[#8681FF] cursor-pointer">
+                Log in instead
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="w-1/2">
+          {/* Section 2 content */}
+          <img src={loginPic} alt="" />
+        </div>
+      </div>
+    </div>
+  </>
   )
 }
 
